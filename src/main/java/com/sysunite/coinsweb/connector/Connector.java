69a374ac89da3457ef68247c95600a0f9ca22459
Bastiaan Bijl <bastiaan@sysunite.com>
2017-04-26 17:27:12 +0200

    Connectors

--- src/main/java/com/sysunite/coinsweb/connector/Connector.java
+++ src/main/java/com/sysunite/coinsweb/connector/Connector.java
@@ -1,10 +1,14 @@
 package com.sysunite.coinsweb.connector;
 
+import java.io.File;
+
 /**
  * @author bastbijl, Sysunite 2017
  */
 public interface Connector {
 
-  boolean connect();
   boolean testConnection();
+  boolean query(String queryString);
+  boolean cleanup();
+  boolean uploadFile(File file, String namespace);
 }

2770132b997879439b72d3b0551b72468ce5bb9d
Bastiaan Bijl <bastiaan@sysunite.com>
2017-09-11 10:42:42 +0200

    Run script examples

---
+++ doc/coins-validator.bat
@@ -0,0 +1,2 @@
+echo off
+java -Xms6g -Xmx8g -jar "C:\Program Files\validator-cli\2.0.7\validator-cli-2.0.7.jar" %*
---
+++ doc/coins-validator.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -Xms6g -Xmx8g -jar /usr/lib/validator-cli/2.0.7/validator-cli-2.0.7.jar "$@"

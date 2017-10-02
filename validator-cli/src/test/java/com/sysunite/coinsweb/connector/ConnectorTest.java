349f082c729940a5c7dc5f9f545fa5fa8d6ec239
Bastiaan Bijl <bastiaan@sysunite.com>
2017-10-02 15:44:42 +0200

    Export rdf test on connector

--- validator-cli/src/test/java/com/sysunite/coinsweb/connector/ConnectorTest.java
+++ validator-cli/src/test/java/com/sysunite/coinsweb/connector/ConnectorTest.java
@@ -1,5 +1,6 @@
 package com.sysunite.coinsweb.connector;
 
+import application.run.HostFiles;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.sysunite.coinsweb.connector.graphdb.GraphDB;
@@ -12,14 +13,17 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
 /**
  * @author bastbijl, Sysunite 2017
  */
-public class ConnectorTest {
+public class ConnectorTest extends HostFiles {
 
   Logger log = LoggerFactory.getLogger(ConnectorTest.class);
 
@@ -33,7 +37,7 @@ public class ConnectorTest {
     ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
     try {
 
-      File configYml = new File(getClass().getClassLoader().getResource("general-9.85-virtuoso.yml").getFile());
+      File configYml = new File(getClass().getClassLoader().getResource("general-9.85.yml").getFile());
       ConfigFile configFile = mapper.readValue(configYml, ConfigFile.class);
 
       Connector connector = new GraphDB(configFile.getEnvironment());
@@ -61,7 +65,7 @@ public class ConnectorTest {
     ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
     try {
 
-      File configYml = new File(getClass().getClassLoader().getResource("general-9.85-graphdb.yml").getFile());
+      File configYml = new File(getClass().getClassLoader().getResource("general-9.85.yml").getFile());
       ConfigFile configFile = mapper.readValue(configYml, ConfigFile.class);
 
       Connector connector = new GraphDB(configFile.getEnvironment());
@@ -72,6 +76,40 @@ public class ConnectorTest {
       List list = connector.listPhiGraphs();
 
       log.info(ReportFactory.buildJson(list));
+
+
+
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+    }
+
+  }
+
+  @Test
+  public void exportRdf() {
+
+    Store.factory = new ConnectorFactoryImpl();
+    StepDeserializer.factory = new StepFactoryImpl();
+    ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+    try {
+
+      File configYml = new File(getClass().getClassLoader().getResource("general-9.85.yml").getFile());
+      ConfigFile configFile = mapper.readValue(configYml, ConfigFile.class);
+
+      Connector connector = new GraphDB(configFile.getEnvironment());
+      log.info(""+connector.testConnection());
+
+
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      HashMap<String, String> namespaceMap = new HashMap<>();
+      namespaceMap.put("otl", "http://otl.rws.nl/otl#");
+      namespaceMap.put("owl", "http://www.w3.org/2002/07/owl#");
+      namespaceMap.put("cbim", "http://www.coinsweb.nl/cbim-2.0.rdf#");
+      ArrayList<String> contexts = new ArrayList<>();
+      contexts.add("http://coins-commander.com/container-main-graph#");
+      connector.writeContextsToFile(contexts, baos, namespaceMap, "http://coins-commander.com/container-main-graph#");
+
+      log.info(baos.toString("UTF-8"));
 
 
 

62ee3e11cccc7b55781378f586f78d8e00f34d0d
Bastiaan Bijl <bastiaan@sysunite.com>
2019-03-08 15:44:15 +0100

    Merge two test files

--- src/test/java/com/sysunite/coinsweb/parser/config/ConfigFileTest.java
+++ src/test/java/com/sysunite/coinsweb/parser/config/ConfigFileTest.java
@@ -1,26 +1,19 @@
 package com.sysunite.coinsweb.parser.config;
 
 import application.run.HostFiles;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.sysunite.coinsweb.connector.Connector;
 import com.sysunite.coinsweb.connector.ConnectorFactoryImpl;
-import com.sysunite.coinsweb.connector.graphdb.GraphDB;
+import com.sysunite.coinsweb.filemanager.DescribeFactoryImpl;
 import com.sysunite.coinsweb.parser.config.factory.ConfigFactory;
 import com.sysunite.coinsweb.parser.config.pojo.ConfigFile;
 import com.sysunite.coinsweb.parser.config.pojo.StepDeserializer;
 import com.sysunite.coinsweb.parser.config.pojo.Store;
-import com.sysunite.coinsweb.report.ReportFactory;
 import com.sysunite.coinsweb.steps.StepFactoryImpl;
-import com.sysunite.coinsweb.steps.ValidationStep;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.List;
 
 /**
  * @author bastbijl, Sysunite 2017
@@ -32,33 +25,46 @@ public class ConfigFileTest extends HostFiles {
   public static void before() {
     Store.factory = new ConnectorFactoryImpl();
     StepDeserializer.factory = new StepFactoryImpl();
+    ConfigFactory.setDescribeFactory(new DescribeFactoryImpl());
   }
 
   @Test
   public void testContainer() {
-    ConfigFile configFile = ConfigFile.parse(new File(getClass().getClassLoader().getResource("general-9.85.yml").getFile()));
+    ConfigFile configFile = ConfigFile.parse(new File(getClass().getResource("general-9.85.yml").getFile()));
     String yml = ConfigFactory.toYml(configFile);
     System.out.println(yml);
   }
 
   @Test
   public void testMinimalContainer() {
-    ConfigFile configFile = ConfigFile.parse(new File(getClass().getClassLoader().getResource("minimal-container.yml").getFile()));
+    ConfigFile configFile = ConfigFile.parse(new File(getClass().getResource("minimal-container.yml").getFile()));
     String yml = ConfigFactory.toYml(configFile);
     System.out.println(yml);
+
+    DescribeFactoryImpl.expandGraphConfig(configFile);
+    String ymlExpanded = ConfigFactory.toYml(configFile);
+    System.out.println(ymlExpanded);
   }
 
   @Test
   public void testMinimalVirtual() {
-    ConfigFile configFile = ConfigFile.parse(new File(getClass().getClassLoader().getResource("minimal-virtual.yml").getFile()));
+    ConfigFile configFile = ConfigFile.parse(new File(getClass().getResource("minimal-virtual.yml").getFile()));
     String yml = ConfigFactory.toYml(configFile);
     System.out.println(yml);
+
+    DescribeFactoryImpl.expandGraphConfig(configFile);
+    String ymlExpanded = ConfigFactory.toYml(configFile);
+    System.out.println(ymlExpanded);
   }
 
   @Test
   public void testVirtualExpandingWildcards() {
-    ConfigFile configFile = ConfigFile.parse(new File(getClass().getClassLoader().getResource("virtual-expanding-wildcards.yml").getFile()));
+    ConfigFile configFile = ConfigFile.parse(new File(getClass().getResource("virtual-expanding-wildcards.yml").getFile()));
     String yml = ConfigFactory.toYml(configFile);
     System.out.println(yml);
+
+    DescribeFactoryImpl.expandGraphConfig(configFile);
+    String ymlExpanded = ConfigFactory.toYml(configFile);
+    System.out.println(ymlExpanded);
   }
 }

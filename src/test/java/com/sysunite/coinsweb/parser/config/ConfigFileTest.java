8604c3711bd358cde11ac55398d969254ee98412
Bastiaan Bijl <bastiaan@sysunite.com>
2019-03-13 22:48:58 +0100

    Update test

--- src/test/java/com/sysunite/coinsweb/parser/config/ConfigFileTest.java
+++ src/test/java/com/sysunite/coinsweb/parser/config/ConfigFileTest.java
@@ -1,10 +1,10 @@
 package com.sysunite.coinsweb.parser.config;
 
-import application.run.HostFiles;
 import com.sysunite.coinsweb.connector.ConnectorFactoryImpl;
 import com.sysunite.coinsweb.filemanager.DescribeFactoryImpl;
 import com.sysunite.coinsweb.parser.config.factory.ConfigFactory;
 import com.sysunite.coinsweb.parser.config.pojo.ConfigFile;
+import com.sysunite.coinsweb.parser.config.pojo.Graph;
 import com.sysunite.coinsweb.parser.config.pojo.StepDeserializer;
 import com.sysunite.coinsweb.parser.config.pojo.Store;
 import com.sysunite.coinsweb.steps.StepFactoryImpl;
@@ -14,12 +14,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author bastbijl, Sysunite 2017
  */
-public class ConfigFileTest extends HostFiles {
+public class ConfigFileTest {
   Logger log = LoggerFactory.getLogger(ConfigFileTest.class);
+
+  private boolean visuallyInspect = false;
 
   @BeforeClass
   public static void before() {
@@ -29,42 +36,171 @@ public class ConfigFileTest extends HostFiles {
   }
 
   @Test
-  public void testContainer() {
-    ConfigFile configFile = ConfigFile.parse(new File(getClass().getResource("general-9.85.yml").getFile()));
-    String yml = ConfigFactory.toYml(configFile);
-    System.out.println(yml);
+  public void voorbeeldContainer() {
+    ConfigFile configFile = ConfigFile.parse(new File(getClass().getResource("voorbeeldcontainer-full.yml").getFile()));
+    ConfigFactory.overrideContainers(configFile, 1, new Path[]{Paths.get("../../../../../voorbeeldcontainer-6.ccr")});
+    inspect(ConfigFactory.toYml(configFile));
+
+    List<Graph> graphs;
+    Graph graph;
+
+    // Before expand
+    graphs = configFile.getRun().getContainers()[0].getGraphs();
+    assertEquals(4, graphs.size());
+    graph = graphs.get(0);
+    assertEquals("bim/*", graph.getSource().getPath());
+    assertEquals("*", graph.getSource().getGraphname());
+    assertEquals("*", graph.getSource().getDefaultFileName());
+    graph = graphs.get(1);
+    assertEquals("bim/repository/*", graph.getSource().getPath());
+    assertEquals("*", graph.getSource().getGraphname());
+    assertEquals("*", graph.getSource().getDefaultFileName());
+
+    DescribeFactoryImpl.expandGraphConfig(configFile);
+
+    // After expand
+    graphs = configFile.getRun().getContainers()[0].getGraphs();
+    assertEquals(4, graphs.size());
+    graph = graphs.get(0);
+    assertEquals("bim/repository/rws-coins-20-referentiekader-21.ttl", graph.getSource().getPath());
+    assertEquals("http://otl.rws.nl/coins2/rws-referentiekader.rdf", graph.getSource().getGraphname());
+    assertEquals("rws-coins-20-referentiekader-21.ttl", graph.getSource().getDefaultFileName());
+    graph = graphs.get(1);
+    assertEquals("bim/repository/coins2.0.rdf", graph.getSource().getPath());
+    assertEquals("http://www.coinsweb.nl/cbim-2.0.rdf", graph.getSource().getGraphname());
+    assertEquals("coins2.0.rdf", graph.getSource().getDefaultFileName());
+    graph = graphs.get(2);
+    assertEquals("bim/testdata_211.ttl", graph.getSource().getPath());
+    assertEquals("http://areaal.rws.nl/S.001032/07032018", graph.getSource().getGraphname());
+    assertEquals("testdata_211.ttl", graph.getSource().getDefaultFileName());
+    graph = graphs.get(3);
+    assertEquals("bim/repository/20180131_otl22_acceptatie_definitief.ttl", graph.getSource().getPath());
+    assertEquals("http://otl.rws.nl/", graph.getSource().getGraphname());
+    assertEquals("20180131_otl22_acceptatie_definitief.ttl", graph.getSource().getDefaultFileName());
+
+    inspect(ConfigFactory.toYml(configFile));
   }
 
   @Test
-  public void testMinimalContainer() {
-    ConfigFile configFile = ConfigFile.parse(new File(getClass().getResource("minimal-container.yml").getFile()));
-    String yml = ConfigFactory.toYml(configFile);
-    System.out.println(yml);
+  public void voorbeeldContainerOnlyCbimFile() {
+    ConfigFile configFile = ConfigFile.parse(new File(getClass().getResource("voorbeeldcontainer-only-cbim-file.yml").getFile()));
+    ConfigFactory.overrideContainers(configFile, 1, new Path[]{Paths.get("../../../../../voorbeeldcontainer-6.ccr")});
+    inspect(ConfigFactory.toYml(configFile));
+
+    List<Graph> graphs;
+    Graph graph;
+
+    // Before expand
+    graphs = configFile.getRun().getContainers()[0].getGraphs();
+    assertEquals(4, graphs.size());
+    graph = graphs.get(0);
+    assertEquals("bim/*", graph.getSource().getPath());
+    assertEquals("*", graph.getSource().getGraphname());
+    assertEquals("*", graph.getSource().getDefaultFileName());
+    graph = graphs.get(1);
+    assertEquals("bim/repository/coins*", graph.getSource().getPath());
+    assertEquals("*", graph.getSource().getGraphname());
+    assertEquals("coins*", graph.getSource().getDefaultFileName());
 
     DescribeFactoryImpl.expandGraphConfig(configFile);
-    String ymlExpanded = ConfigFactory.toYml(configFile);
-    System.out.println(ymlExpanded);
+
+    // After expand
+    graphs = configFile.getRun().getContainers()[0].getGraphs();
+    assertEquals(2, graphs.size());
+    graph = graphs.get(0);
+    assertEquals("bim/repository/coins2.0.rdf", graph.getSource().getPath());
+    assertEquals("http://www.coinsweb.nl/cbim-2.0.rdf", graph.getSource().getGraphname());
+    assertEquals("coins2.0.rdf", graph.getSource().getDefaultFileName());
+    graph = graphs.get(1);
+    assertEquals("bim/testdata_211.ttl", graph.getSource().getPath());
+    assertEquals("http://areaal.rws.nl/S.001032/07032018", graph.getSource().getGraphname());
+    assertEquals("testdata_211.ttl", graph.getSource().getDefaultFileName());
+
+    inspect(ConfigFactory.toYml(configFile));
   }
 
   @Test
-  public void testMinimalVirtual() {
-    ConfigFile configFile = ConfigFile.parse(new File(getClass().getResource("minimal-virtual.yml").getFile()));
-    String yml = ConfigFactory.toYml(configFile);
-    System.out.println(yml);
+  public void voorbeeldContainerOnlyCbimNamespace() {
+    ConfigFile configFile = ConfigFile.parse(new File(getClass().getResource("voorbeeldcontainer-only-cbim-namespace.yml").getFile()));
+    ConfigFactory.overrideContainers(configFile, 1, new Path[]{Paths.get("../../../../../voorbeeldcontainer-6.ccr")});
+    inspect(ConfigFactory.toYml(configFile));
+
+    List<Graph> graphs;
+    Graph graph;
+
+    // Before expand
+    graphs = configFile.getRun().getContainers()[0].getGraphs();
+    assertEquals(4, graphs.size());
+    graph = graphs.get(0);
+    assertEquals("bim/*", graph.getSource().getPath());
+    assertEquals("*", graph.getSource().getGraphname());
+    assertEquals("*", graph.getSource().getDefaultFileName());
+    graph = graphs.get(1);
+    assertEquals("bim/repository/*", graph.getSource().getPath());
+    assertEquals("http://www.coinsweb.nl/cbim-2.0.rdf", graph.getSource().getGraphname());
+    assertEquals("*", graph.getSource().getDefaultFileName());
 
     DescribeFactoryImpl.expandGraphConfig(configFile);
-    String ymlExpanded = ConfigFactory.toYml(configFile);
-    System.out.println(ymlExpanded);
+
+    // After expand
+    graphs = configFile.getRun().getContainers()[0].getGraphs();
+    assertEquals(2, graphs.size());
+    graph = graphs.get(0);
+    assertEquals("bim/repository/coins2.0.rdf", graph.getSource().getPath());
+    assertEquals("http://www.coinsweb.nl/cbim-2.0.rdf", graph.getSource().getGraphname());
+    assertEquals("coins2.0.rdf", graph.getSource().getDefaultFileName());
+    graph = graphs.get(1);
+    assertEquals("bim/testdata_211.ttl", graph.getSource().getPath());
+    assertEquals("http://areaal.rws.nl/S.001032/07032018", graph.getSource().getGraphname());
+    assertEquals("testdata_211.ttl", graph.getSource().getDefaultFileName());
+
+    inspect(ConfigFactory.toYml(configFile));
   }
 
   @Test
-  public void testVirtualExpandingWildcards() {
-    ConfigFile configFile = ConfigFile.parse(new File(getClass().getResource("virtual-expanding-wildcards.yml").getFile()));
-    String yml = ConfigFactory.toYml(configFile);
-    System.out.println(yml);
+  public void testVirtual() {
+    ConfigFile configFile = ConfigFile.parse(new File(getClass().getResource("virtual.yml").getFile()));
+    inspect(ConfigFactory.toYml(configFile));
+
+    List<Graph> graphs;
+    Graph graph;
+
+    // Before expand
+    graphs = configFile.getRun().getContainers()[0].getGraphs();
+    assertEquals(2, graphs.size());
+    graph = graphs.get(0);
+    assertEquals("nquad.nq", graph.getSource().getPath());
+    assertEquals("*", graph.getSource().getGraphname());
+    assertEquals("nquad.nq", graph.getSource().getDefaultFileName());
+    graph = graphs.get(1);
+    assertEquals("empty.rdf", graph.getSource().getPath());
+    assertEquals("*", graph.getSource().getGraphname());
+    assertEquals("empty.rdf", graph.getSource().getDefaultFileName());
 
     DescribeFactoryImpl.expandGraphConfig(configFile);
-    String ymlExpanded = ConfigFactory.toYml(configFile);
-    System.out.println(ymlExpanded);
+
+    // After expand
+    graphs = configFile.getRun().getContainers()[0].getGraphs();
+    assertEquals(3, graphs.size());
+    graph = graphs.get(0);
+    assertEquals("nquad.nq", graph.getSource().getPath());
+    assertEquals("http://example.org/graphs/superman", graph.getSource().getGraphname());
+    assertEquals("nquad.nq", graph.getSource().getDefaultFileName());
+    graph = graphs.get(1);
+    assertEquals("empty.rdf", graph.getSource().getPath());
+    assertEquals("http://www.coinsweb.nl/empty.rdf", graph.getSource().getGraphname());
+    assertEquals("empty.rdf", graph.getSource().getDefaultFileName());
+    graph = graphs.get(2);
+    assertEquals("nquad.nq", graph.getSource().getPath());
+    assertEquals("http://example.org/graphs/spiderman", graph.getSource().getGraphname());
+    assertEquals("nquad.nq", graph.getSource().getDefaultFileName());
+
+    inspect(ConfigFactory.toYml(configFile));
+  }
+
+  private void inspect(String message) {
+    if(visuallyInspect) {
+      System.out.println(message);
+    }
   }
 }

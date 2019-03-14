4d3ba5a0148bd84ca6063574836f6c8cba707bea
Bastiaan Bijl <bastiaan@sysunite.com>
2019-03-14 15:51:10 +0100

    Fix test

--- src/test/java/com/sysunite/coinsweb/parser/config/ConfigFileTest.java
+++ src/test/java/com/sysunite/coinsweb/parser/config/ConfigFileTest.java
@@ -1,12 +1,10 @@
 package com.sysunite.coinsweb.parser.config;
 
+import com.sun.corba.se.impl.orbutil.graph.GraphImpl;
 import com.sysunite.coinsweb.connector.ConnectorFactoryImpl;
 import com.sysunite.coinsweb.filemanager.DescribeFactoryImpl;
 import com.sysunite.coinsweb.parser.config.factory.ConfigFactory;
-import com.sysunite.coinsweb.parser.config.pojo.ConfigFile;
-import com.sysunite.coinsweb.parser.config.pojo.Graph;
-import com.sysunite.coinsweb.parser.config.pojo.StepDeserializer;
-import com.sysunite.coinsweb.parser.config.pojo.Store;
+import com.sysunite.coinsweb.parser.config.pojo.*;
 import com.sysunite.coinsweb.steps.StepFactoryImpl;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -26,7 +24,7 @@ import static org.junit.Assert.assertEquals;
 public class ConfigFileTest {
   Logger log = LoggerFactory.getLogger(ConfigFileTest.class);
 
-  private boolean visuallyInspect = false;
+  private boolean visuallyInspect = true;
 
   @BeforeClass
   public static void before() {
@@ -60,20 +58,26 @@ public class ConfigFileTest {
 
     // After expand
     graphs = configFile.getRun().getContainers()[0].getGraphs();
-    assertEquals(4, graphs.size());
+    assertEquals(6, graphs.size());
     graph = graphs.get(0);
+    assertEquals(new GraphVarImpl("INSTANCE_UNION_GRAPH"), graph.getSource().getGraph());
+    assertEquals("file.2.rdf", graph.getSource().getDefaultFileName());
+    graph = graphs.get(1);
+    assertEquals(new GraphVarImpl("SCHEMA_UNION_GRAPH"), graph.getSource().getGraph());
+    assertEquals("file.3.rdf", graph.getSource().getDefaultFileName());
+    graph = graphs.get(2);
     assertEquals("bim/repository/rws-coins-20-referentiekader-21.ttl", graph.getSource().getPath());
     assertEquals("http://otl.rws.nl/coins2/rws-referentiekader.rdf", graph.getSource().getGraphname());
     assertEquals("rws-coins-20-referentiekader-21.ttl", graph.getSource().getDefaultFileName());
-    graph = graphs.get(1);
+    graph = graphs.get(3);
     assertEquals("bim/repository/coins2.0.rdf", graph.getSource().getPath());
     assertEquals("http://www.coinsweb.nl/cbim-2.0.rdf", graph.getSource().getGraphname());
     assertEquals("coins2.0.rdf", graph.getSource().getDefaultFileName());
-    graph = graphs.get(2);
+    graph = graphs.get(4);
     assertEquals("bim/testdata_211.ttl", graph.getSource().getPath());
     assertEquals("http://areaal.rws.nl/S.001032/07032018", graph.getSource().getGraphname());
     assertEquals("testdata_211.ttl", graph.getSource().getDefaultFileName());
-    graph = graphs.get(3);
+    graph = graphs.get(5);
     assertEquals("bim/repository/20180131_otl22_acceptatie_definitief.ttl", graph.getSource().getPath());
     assertEquals("http://otl.rws.nl/", graph.getSource().getGraphname());
     assertEquals("20180131_otl22_acceptatie_definitief.ttl", graph.getSource().getDefaultFileName());
@@ -106,12 +110,18 @@ public class ConfigFileTest {
 
     // After expand
     graphs = configFile.getRun().getContainers()[0].getGraphs();
-    assertEquals(2, graphs.size());
+    assertEquals(4, graphs.size());
     graph = graphs.get(0);
+    assertEquals(new GraphVarImpl("INSTANCE_UNION_GRAPH"), graph.getSource().getGraph());
+    assertEquals("file.2.rdf", graph.getSource().getDefaultFileName());
+    graph = graphs.get(1);
+    assertEquals(new GraphVarImpl("SCHEMA_UNION_GRAPH"), graph.getSource().getGraph());
+    assertEquals("file.3.rdf", graph.getSource().getDefaultFileName());
+    graph = graphs.get(2);
     assertEquals("bim/repository/coins2.0.rdf", graph.getSource().getPath());
     assertEquals("http://www.coinsweb.nl/cbim-2.0.rdf", graph.getSource().getGraphname());
     assertEquals("coins2.0.rdf", graph.getSource().getDefaultFileName());
-    graph = graphs.get(1);
+    graph = graphs.get(3);
     assertEquals("bim/testdata_211.ttl", graph.getSource().getPath());
     assertEquals("http://areaal.rws.nl/S.001032/07032018", graph.getSource().getGraphname());
     assertEquals("testdata_211.ttl", graph.getSource().getDefaultFileName());
@@ -144,12 +154,18 @@ public class ConfigFileTest {
 
     // After expand
     graphs = configFile.getRun().getContainers()[0].getGraphs();
-    assertEquals(2, graphs.size());
+    assertEquals(4, graphs.size());
     graph = graphs.get(0);
+    assertEquals(new GraphVarImpl("INSTANCE_UNION_GRAPH"), graph.getSource().getGraph());
+    assertEquals("file.2.rdf", graph.getSource().getDefaultFileName());
+    graph = graphs.get(1);
+    assertEquals(new GraphVarImpl("SCHEMA_UNION_GRAPH"), graph.getSource().getGraph());
+    assertEquals("file.3.rdf", graph.getSource().getDefaultFileName());
+    graph = graphs.get(2);
     assertEquals("bim/repository/coins2.0.rdf", graph.getSource().getPath());
     assertEquals("http://www.coinsweb.nl/cbim-2.0.rdf", graph.getSource().getGraphname());
     assertEquals("coins2.0.rdf", graph.getSource().getDefaultFileName());
-    graph = graphs.get(1);
+    graph = graphs.get(3);
     assertEquals("bim/testdata_211.ttl", graph.getSource().getPath());
     assertEquals("http://areaal.rws.nl/S.001032/07032018", graph.getSource().getGraphname());
     assertEquals("testdata_211.ttl", graph.getSource().getDefaultFileName());
